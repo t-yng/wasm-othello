b9d195592cffd1f89ccf9a6513bccc2989b48802
t-yng <t.yng.greenmonkey@gmail.com>
2020-03-20 17:05:06 +0900

    AIの思考時間を記録する処理を追加

--- src/lib/ai/ai.ts
+++ src/lib/ai/ai.ts
@@ -4,14 +4,23 @@ import { Player } from "../othello/player";
 
 export abstract class AI extends Player {
     protected _level: number;
+    private _times: number[];
 
     constructor (stone: Stone, level: number) {
       super(stone);
       this._level = level;
+      this._times = [];
+    }
+
+    get times() {
+      return this._times
     }
 
     putStone(cells: Cell[], stone: Stone) {
+      const start = performance.now();
       const idx = this.choiceNextPosition(cells, stone);
+      const end = performance.now();
+      this._times.push(end-start);
       this.select(idx);
     }
 
