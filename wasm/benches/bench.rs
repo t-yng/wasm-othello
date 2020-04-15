11e71f4e88456cad0d3e77c2ef219334ba43633c
t-yng <t.yng.greenmonkey@gmail.com>
2020-04-15 23:18:25 +0900

    ベンチマーク実行のソースを追加

---
+++ wasm/benches/bench.rs
@@ -0,0 +1,27 @@
+#![feature(test)]
+
+extern crate test;
+extern crate wasm_othello;
+// extern crate cell;
+
+use wasm_othello::ai::minmax::MinMax;
+use wasm_othello::cell::Stone;
+
+#[bench]
+fn choice_next_position(b: &mut test::Bencher) {
+    let minmax = MinMax::new(6);
+    let cells: Vec<u8> = vec![
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 1, 1, 0, 0, 0,
+        0, 0, 0, 2, 2, 2, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+
+    b.iter(|| {
+        minmax.choice_next_position(&cells, Stone::WHITE);
+    });
+}
