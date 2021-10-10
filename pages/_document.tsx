f0dd3f7ad7fe3e8a748a49db8d16518f7ddc120b
t-yng <t.yng.greenmonkey@gmail.com>
2021-10-11 01:56:07 +0900

    _documentを修正

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,44 @@
-import React from 'react';
-import Document, { Head, Main, NextScript, DocumentContext } from 'next/document';
-import { extractStyles } from 'evergreen-ui';
+import React from "react";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { extractStyles } from "evergreen-ui";
 
 type DocumentProps = {
-    css: string;
-    hydrationScript: JSX.Element;
-}
+  css: string;
+  hydrationScript: JSX.Element;
+};
 
 export default class MyDocument extends Document<DocumentProps> {
-    static async getInitialProps(ctx: DocumentContext) {
-        const initialProps = await Document.getInitialProps(ctx);
-        const { css, hydrationScript } = extractStyles();
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const { css, hydrationScript } = extractStyles();
 
-        return {
-            ...initialProps,
-              css,
-              hydrationScript,
-        };
-    }
+    return {
+      ...initialProps,
+      css,
+      hydrationScript,
+    };
+  }
 
-    render() {
-        const { css, hydrationScript } = this.props;
+  render() {
+    const { css, hydrationScript } = this.props;
 
-        return (
-          <html>
-            <Head>
-                {/* Global site tag (gtag.js) - Google Analytics */}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-57390966-5"></script>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html:`
+    return (
+      <Html>
+        <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-57390966-5"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
@@ -37,19 +46,18 @@ export default class MyDocument extends Document<DocumentProps> {
                             gtag('config', 'UA-57390966-5', {
                                 page_path: window.location.pathname,
                             });
-                        `
-                    }}
-                />
-                <style dangerouslySetInnerHTML={{ __html: css }} />
-            </Head>
+                        `,
+            }}
+          />
+          <style dangerouslySetInnerHTML={{ __html: css }} />
+        </Head>
 
-            <body>
-              <Main />
-              {hydrationScript}
-              <NextScript />
-            </body>
-          </html>
-        );
-    }
-
+        <body>
+          <Main />
+          {hydrationScript}
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
