f47fe32b7848550d6bdd2c1ec29eadbccd939170
t-yng <t-yng@users.noreply.github.com>
2021-10-11 21:07:03 +0900

    wasmのビルドスクリプトを修正 (#35)
    * wasmのビルドスクリプトを修正
    
    * ラベル修正
    
    * Node.jsのバージョンを変更
    
    * package-lock.json を削除

--- bin/build-wasm.sh
+++ bin/build-wasm.sh
@@ -2,20 +2,21 @@
 
 # Rustが存在しないならインストール
 if !(type "rustc" > /dev/null 2>&1); then
-    echo "start install rust"
+    echo "⏬ install rust"
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     . $HOME/.cargo/env
 else
-    echo "rust is already installed 👍"
+    echo "👍 rust is already installed"
 fi
 
 # wasm-packが存在しないならインストール
 if !(type "wasm-pack" > /dev/null 2>&1); then
-    echo "start install wasm-pack"
+    echo "⏬ install wasm-pack"
     curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 else
-    echo "wasm-pack is already installed 👍"
+    echo "👍 wasm-pack is already installed"
 fi
 
 # wasmのビルド
+echo "🛠 build wasm"
 wasm-pack build wasm -d ../src/lib/wasm;
